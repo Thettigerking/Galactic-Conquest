1fe7b61aaa5301e3b24b323bb03b1cf4434bef6a
Brickmasterhunt <brickmasterlogan@icloud.com>
2025-06-01 13:48:34 -0400

    buildsystem works now

--- common/src/main/java/com/thetigerking/galacticconquest/Galacticconquest.java
+++ common/src/main/java/com/thetigerking/galacticconquest/Galacticconquest.java
@@ -1,7 +1,5 @@
 package com.thetigerking.galacticconquest;
 
-import com.lowdragmc.lowdraglib.LDLib;
-import com.lowdragmc.photon.Photon;
 
 public final class Galacticconquest {
     public static final String MOD_ID = "galacticconquest";
