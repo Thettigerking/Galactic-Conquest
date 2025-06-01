1c53bcf5c23f606cbf1c6bb05b13b415723bbf39
Brickmasterhunt <brickmasterlogan@icloud.com>
2025-06-01 19:50:30 -0400

    some stuff

--- common/src/main/java/com/thetigerking/galacticconquest/Galacticconquest.java
+++ common/src/main/java/com/thetigerking/galacticconquest/Galacticconquest.java
@@ -1,10 +1,18 @@
 package com.thetigerking.galacticconquest;
 
+import com.thetigerking.galacticconquest.event.AtmosphereCollision;
+import dev.architectury.event.events.common.TickEvent;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.server.level.ServerLevel;
 
 public final class Galacticconquest {
     public static final String MOD_ID = "galacticconquest";
 
     public static void init() {
+        TickEvent.ServerLevelTick.SERVER_LEVEL_POST.register((ServerLevel serverLevel) -> {
+            AtmosphereCollision.onShipCollide();
+        });
 
 
     }
---
+++ common/src/main/java/com/thetigerking/galacticconquest/event/AtmosphereCollision.java
@@ -0,0 +1,7 @@
+package com.thetigerking.galacticconquest.event;
+
+public class AtmosphereCollision {
+    static public void onShipCollide() {
+
+    }
+}
