7ca263a626a59adfc3a050aa5a781becf3aead47
Brickmasterhunt <brickmasterlogan@icloud.com>
2025-05-31 23:05:12 -0400

    working on buildsystem

--- common/src/main/java/com/thetigerking/galacticconquest/Galacticconquest.java
+++ common/src/main/java/com/thetigerking/galacticconquest/Galacticconquest.java
@@ -1,9 +1,13 @@
 package com.thetigerking.galacticconquest;
 
+import com.lowdragmc.lowdraglib.LDLib;
+import com.lowdragmc.photon.Photon;
+
 public final class Galacticconquest {
     public static final String MOD_ID = "galacticconquest";
 
     public static void init() {
-        // Write common init code here.
+
+
     }
 }
