aace95ce8536f59fe3c47c1ee699a490421ce4f9
Tiger (Animesh) <TheTTigerKing@gmail.com>
2025-06-02 21:25:35 -0400

    Fixed solar system dim, added mars biome, working on adding blocks

--- common/src/main/java/com/thetigerking/galacticconquest/Galacticconquest.java
+++ common/src/main/java/com/thetigerking/galacticconquest/Galacticconquest.java
@@ -11,7 +11,10 @@ public final class Galacticconquest {
 
     public static void init() {
         TickEvent.ServerLevelTick.SERVER_LEVEL_POST.register((ServerLevel serverLevel) -> {
-            AtmosphereCollision.onShipCollide();
+        // makes sure it doesn't run multiple times for tick, error for some reason, idk why
+         /*   if (event.phase != TickEvent.Phase.END) {
+               return; */
+                AtmosphereCollision.onShipCollide();
         });
 
 
---
+++ common/src/main/java/com/thetigerking/galacticconquest/blocks/galacticconquestBlocks.java
@@ -0,0 +1,10 @@
+package com.thetigerking.galacticconquest.blocks;
+
+import dev.architectury.registry.registries.DeferredRegister;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.world.level.block.Block;
+
+public class galacticconquestBlocks {
+
+    private static final DeferredRegister<Block> blocks = DeferredRegister.create(galaticconquest.MOD_ID, Registries.BLOCK);
+}
